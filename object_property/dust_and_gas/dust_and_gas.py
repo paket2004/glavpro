d0557c190efc7bca12203b850ebb5dc75db44f94
Ilia Mitrokhin <i.mitrokhin@innopolis.university>
2025-04-16 18:31:59 +0300

    font and style correction

--- object_property/dust_and_gas/dust_and_gas.py
+++ object_property/dust_and_gas/dust_and_gas.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from docx import Document
-
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 doc = Document()
 st.title("Характеристика пылегазоочистного оборудования и оценка его эффективности")
 
@@ -13,6 +13,7 @@ if st.button("Отправить"):
         # 1.2.3 Характеристика пылегазоочистного оборудования и оценка его эффективности
         par = doc.add_paragraph()
         par.add_run('''1.2.3 Характеристика пылегазоочистного оборудования и оценка его эффективности''').bold = True
+        par.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
         doc.add_paragraph("Пылегазоочистное оборудование (ПГОУ) на объекте негативного воздействия отсутствуют.")
         doc.save("object_property/dust_and_gas/dust_and_gus_info.docx")
         print("saved succesfully")
