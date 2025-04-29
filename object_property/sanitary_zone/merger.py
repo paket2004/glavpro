08216e67b1f590b20495037a1949fbe6e224d26e
paket2004 <mitrohin_ilya@bk.ru>
2025-04-29 09:48:50 +0300

    new merger

--- object_property/sanitary_zone/merger.py
+++ object_property/sanitary_zone/merger.py
@@ -1,16 +1,33 @@
+from docxcompose.composer import Composer
+from docx import Document as Document_compose
 from docx import Document
+from docx.oxml import OxmlElement
+from docx.oxml.ns import qn
 
-def merge_docx(files, output_file):
-    merged_document = Document(files[0])  # Start with the first document
+def add_page_break(doc):
+    """Inserts a page break into the document."""
+    paragraph = doc.add_paragraph()
+    run = paragraph.add_run()
+    br = OxmlElement('w:br')
+    br.set(qn('w:type'), 'page')
+    run._r.append(br)
 
-    for file in files[1:]:  # Merge the rest
-        doc = Document(file)
-        for element in doc.element.body:
-            merged_document.element.body.append(element)
-
-    merged_document.save(output_file)
-    print(f"Documents merged successfully into {output_file}")
-
-# Example usage
-files_to_merge = ["sanitary_zone/description.docx", "sanitary_zone/sanpin_table.docx"]
-merge_docx(files_to_merge, "sanitary_zone/sanitary_zone.docx")
+def combine_all_docx(filename_master,files_list):
+    number_of_sections=len(files_list)
+    master = Document_compose(filename_master)
+    composer = Composer(master)
+    for i in range(0, number_of_sections):
+        print("sth")
+        doc_temp = Document_compose(files_list[i])
+        composer.append(doc_temp)
+    composer.save("object_property\sanitary_zone\sanitary_zone.docx")
+files_to_merge = [
+    "object_property\sanitary_zone\description.docx",
+    "object_property\dust_and_gas\dust_and_gus_info.docx",
+    "object_property\previous_inventarization\prev_inv_info.docx"
+]
+# filename_master="report.docx"
+document = Document()
+document.save("emptyfile.docx")
+filename_master="emptyfile.docx"
+combine_all_docx(filename_master,files_to_merge)
