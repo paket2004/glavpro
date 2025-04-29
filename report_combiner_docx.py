f210efa61b49be38a825bbb7c36154c7597b7b28
paket2004 <mitrohin_ilya@bk.ru>
2025-04-29 09:51:25 +0300

    update report combiner

--- report_combiner_docx.py
+++ report_combiner_docx.py
@@ -4,40 +4,36 @@ from docx import Document
 from docx.oxml import OxmlElement
 from docx.oxml.ns import qn
 
-def add_page_break(doc):
-    """Inserts a page break into the document."""
-    paragraph = doc.add_paragraph()
+def create_page_break_doc():
+    """Creates a temporary document with a page break."""
+    temp_doc = Document()
+    paragraph = temp_doc.add_paragraph()
     run = paragraph.add_run()
     br = OxmlElement('w:br')
     br.set(qn('w:type'), 'page')
     run._r.append(br)
+    return temp_doc
 
-def combine_all_docx(filename_master,files_list):
-    number_of_sections=len(files_list)
+def combine_all_docx(filename_master, files_list):
     master = Document_compose(filename_master)
     composer = Composer(master)
-    for i in range(0, number_of_sections):
-        print("sth")
-        doc_temp = Document_compose(files_list[i])
+
+    # Добавим разрыв страницы после мастер-документа
+    composer.append(create_page_break_doc())
+
+    for i, file_path in enumerate(files_list):
+        doc_temp = Document_compose(file_path)
         composer.append(doc_temp)
+
+        # Вставляем разрыв страницы после каждого документа, кроме последнего
         if i < len(files_list) - 1:
-            add_page_break(composer.doc)
-        
+            composer.append(create_page_break_doc())
+
     composer.save("combined_file.docx")
 
-# files_to_merge = [
-#     "content/Содержание_отчета.docx",
-#     "termins_and_short/dictionary_table.docx",
-#     "introduction/introduction.docx",
-#     "inventarization_description\izav_info\organization_sources_info.docx",
-#     "object_property/dust_and_gas/dust_and_gus_info.docx",
-#     "object_property\previous_inventarization\prev_inv_info.docx",
-#     "object_property/sanitary_zone/sanitary_zone.docx",
-#     "calculations/tables/razdel3/razdel3.docx"
-# ]
+
 
 files_to_merge = [
-    "content/Содержание_отчета.docx",
     "termins_and_short/dictionary_table.docx",
     "introduction/introduction.docx",
     "inventarization_description\punkt1\organization_sources_info.docx",
@@ -46,7 +42,5 @@ files_to_merge = [
     r"calculations\tables\razdel3\razdel3.docx"
 ]
 # filename_master="report.docx"
-document = Document()
-document.save("emptyfile.docx")
-filename_master="emptyfile.docx"
+filename_master="content/Содержание_отчета.docx"
 combine_all_docx(filename_master,files_to_merge)
