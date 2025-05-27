76cb5468eeb110671d8dc2a69de2f99c6e878f0a
paket2004 <mitrohin_ilya@bk.ru>
2025-05-27 12:02:14 +0300

    update requirements and delete redundant stuff

--- pages/kotel_output.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-# Retrieve data from session state
-if "number_of_boilers" in st.session_state and "boiler" in st.session_state:
-    st.write(f"Name: {st.session_state['number_of_boilers']}")
-    st.write(f"Age: {st.session_state['boiler']}")
-else:
-    st.warning("No data found. Please go to the Input Page and enter your information.")
