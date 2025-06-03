6f1800a36e05b368736007c167f3e88d78db2a51
paket2004 <mitrohin_ilya@bk.ru>
2025-06-03 11:04:49 +0300

    path changes for streamlit

--- calculations/tables/fill_table_3_8.py
+++ calculations/tables/fill_table_3_8.py
@@ -1,7 +1,9 @@
 from docx import Document
+import os 
 
 def fill_3_8_table(number, izav_type, izav_amount, izav_speed, fuel_type, work_time_season, work_time_year, nitric_ocide_1_year_warm, nitric_ocide_year_warm, sulfur_year_warm, CO_year_warm, petrol_year_warm, nitric_ocide_1_sec_warm, nitric_ocide_sec_warm, sulfur_sec_warm, CO_sec_warm, petrol_sec_warm):
-    doc = Document("calculations/tables/3_8.docx")
+    path = os.path.join(os.getcwd(), "calculations", "tables", "3_8.docx")
+    doc = Document(path)
     table = doc.tables[0]
     table.cell(2,0).text = str(number)
     table.cell(2,1).text = izav_type
@@ -21,4 +23,5 @@ def fill_3_8_table(number, izav_type, izav_amount, izav_speed, fuel_type, work_t
     table.add_row()
     table.cell(4,0).merge(table.cell(4,10))
     table.cell(4,0).text = """Примечание. Список использованных расчетных методик: 0001 - Методика проведения инвентаризации выбросов ЗВ в атмосферу для автотранспортных предприя-тий". - М., НИИАТ, 1998г. с учётом "Дополнений к методике" ( М., НИИАТ, 1999г.) и Методики…для баз дорожной техники". - М., НИИАТ, 1999г """
-    doc.save("calculations/tables/razdel3/3_8.docx")    
+    path_to_save = os.path.join(os.getcwd(), "calculations", "tables", "razdel3", "3_8.docx")
+    doc.save(path_to_save)    
